fe023d1902abc99aa6003d4d1e9d9d81c4527c6e
earslan25 <92232976+earslan25@users.noreply.github.com>
2023-12-11 00:32:10 -0500

    Update merfNet.py

--- merfNet.py
+++ merfNet.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 from torchvision import transforms
-from scene.cameras import Camera as GSCamera
+from scene.cameras import Camera, MiniCam
+from utils.graphics_utils import getProjectionMatrix
 
 
 class MerfNet(nn.Module):
@@ -17,9 +18,12 @@ class MerfNet(nn.Module):
             transforms.Resize((256, 256)),
             # transforms.CenterCrop(227),
         ))
-        self.camera = camera
-        # optimize the camera translation
-        self.camera_pos = nn.Parameter(torch.from_numpy(np.array(camera.T, dtype=np.float32)).to(device))
+        R = camera.R
+        T = camera.T
+        new_mini_cam = fromGS2MiniCam(camera)
+        self.camera = new_mini_cam
+        # optimize the camera pos
+        self.camera_pos = nn.Parameter(torch.from_numpy(np.array([2, 2, 2], dtype=np.float32)).to(self.device))
         self.resmodel = resmodel.to(device)
         for param in self.resmodel.parameters():
             param.requires_grad = False
@@ -27,7 +31,7 @@ class MerfNet(nn.Module):
         self.loss_fn = loss_fn
 
     def forward(self):
-        self.update_camera()
+        self.update_mini_camera()
         image = self.renderer(self.camera, self.gaussians)
         i = image
         # i = image[0, :, :, :3]
@@ -38,7 +42,29 @@ class MerfNet(nn.Module):
 
         return loss, image, prediction
 
-    def update_camera(self):
+    def update_mini_camera(self):
+        R = look_at_rotation(self.camera_pos[None, :], device=self.device)
+        T = -torch.bmm(R.transpose(1, 2), self.camera_pos[None, :, None])[:, :, 0]
+        R = R.squeeze(0)
+        world_view_transform = getWorld2View2Torch(R, T).transpose(0, 1)
+        projection_matrix = getProjectionMatrix(
+            znear=self.camera.znear, zfar=self.camera.zfar, 
+            fovX=self.camera.FoVx, fovY=self.camera.FoVy).transpose(0,1).to(self.device)
+        full_proj_transform = (world_view_transform.unsqueeze(0).bmm(projection_matrix.unsqueeze(0))).squeeze(0)
+        old_camera = self.camera
+        self.camera = MiniCam(
+            old_camera.image_width,
+            old_camera.image_height,
+            old_camera.FoVx,
+            old_camera.FoVy,
+            old_camera.znear,
+            old_camera.zfar,
+            world_view_transform,
+            full_proj_transform
+        )
+
+
+    def update_GS_camera(self):
         R = look_at_rotation(self.camera_pos[None, :], device=self.device)
         T = -torch.bmm(R.transpose(1, 2), self.camera_pos[None, :, None])[:, :, 0]
         R = R.squeeze(0)
@@ -122,3 +148,31 @@ def format_tensor(
 
     input = input.to(device=device)
     return input
+
+def fromGS2MiniCam(camera):
+    new_mini_cam = MiniCam(
+        camera.image_width,
+        camera.image_height,
+        camera.FoVx,
+        camera.FoVy,
+        camera.znear,
+        camera.zfar,
+        camera.world_view_transform,
+        camera.full_proj_transform
+    )
+    return new_mini_cam
+
+def getWorld2View2Torch(R, t, translate=torch.tensor([.0, .0, .0]), scale=1.0, device="cuda"):
+    translate = translate.to(device)
+    Rt = torch.zeros((4, 4), dtype=torch.float32, device=device)
+    Rt[:3, :3] = R.transpose(0, 1)  
+    Rt[:3, 3] = t
+    Rt[3, 3] = 1.0
+
+    C2W = torch.inverse(Rt)
+    cam_center = C2W[:3, 3]
+    cam_center = (cam_center + translate) * scale
+    C2W[:3, 3] = cam_center
+    Rt = torch.inverse(C2W)
+    return Rt
+
